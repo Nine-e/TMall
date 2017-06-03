dbbd2293064c9c0522923a76359bb18eedec1e71
Nine-e <3287746498@qq.com>
2017-06-03 22:24:49 +0800

    热门品牌

--- js/index.js
+++ js/index.js
@@ -26,6 +26,10 @@ function initPage(){
 		var hotBrandHtml = $.templates("#hotBrandTmpl").render(hotBrandData);
 		$(".tmall").append(hotBrandHtml);
 
+		pinkFloorData = data.pinkFloor;
+		var pinkFloorHtml = $.templates("#pinkFloorTmpl").render(pinkFloorData);
+		$(".tmall").append(pinkFloorHtml);
+
 		footer1Data = data.footer1;
 		var footer1Html = $.templates("#footerTmplOne").render(footer1Data);
 		$(".tmall").append(footer1Html);
@@ -34,18 +38,40 @@ function initPage(){
 		var footer2Html = $.templates("#footerTmplTwo").render(footer2Data);
 		$(".tmall").append(footer2Html);
 
+		/*$("#myCarousel").carousel('cycle');
+        $("#myCarousel").carousel({interval:200});*/
+
+        initCarousel();//回调
+        mask();
 
 	});
 
 }
 
-$(function(){
+function initCarousel(){
         // 初始化轮播
-        $(".start-slide").click(function(){
             $("#myCarousel").carousel('cycle');
-            $("#myCarousel").carousel({
-            	interval:2000
-            });
-        });
-});
+            $("#myCarousel").carousel({interval:20});
+}
+
+function mask(){
+	//处理mask\cover交互
+	$(".mask").hide();
+	$(".ui-hotBrand-slive-item").mouseover(function(){
+		$(this).children().show();
+		var newsrc = $(this).find("img")[0].src;//难点1
+		/*console.log($("#gliveImg")[0].src);*/
+		$("#gliveImg").attr('src',newsrc);
+		/*console.log(newsrc);*/
+	});
+	$(".ui-hotBrand-slive-item").mouseout(function(){
+		$(".ui-hotBrand-slive-cover").hide();
+	});
+	$(".ui-hotBrand-brand-item").mouseover(function(){
+		$(this).children().show();
+	});
+	$(".ui-hotBrand-brand-item").mouseout(function(){
+		$(".ui-hotBrand-brand-item-cover").hide();
+	});
+}
 
